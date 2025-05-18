5de04557c94f49aaed9a39817a9c682a09b3941d
Ta Chi Nguyen <tcnguyen93@gmail.com>
2025-05-18 22:29:23 +0700

    up

--- crud-admin/src/dataProvider.js
+++ crud-admin/src/dataProvider.js
@@ -1,6 +1,7 @@
 // src/dataProvider.js
 import { fetchUtils } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
-
-const dataProvider = simpleRestProvider('https://localhost:7117/api');
+const url = "https://apiketoan.tachinguyen.net/api";
+// const url = "https://localhost:7117/api";
+const dataProvider = simpleRestProvider(url);
 export default dataProvider;
